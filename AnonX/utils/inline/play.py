df2cf37ef510cb18760a796be4bae6d70186aa41
ashishking2006 <134121306+ashishking2006@users.noreply.github.com>
2023-08-30 20:36:26 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -63,7 +63,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/BRANDRD_21"
+                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/@BWF_KING2"
             )
         ],
         [
